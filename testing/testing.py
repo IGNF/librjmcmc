64887ab4eceaf472038d80f54b287b35943530a4
dboldo@6a04c5be-4793-4b4c-94f5-c18864c33b64 <dboldo@6a04c5be-4793-4b4c-94f5-c18864c33b64>
2008-11-14 15:44:06 +0000

    En cours
    --HG--
    extra : convert_revision : svn%3A6a04c5be-4793-4b4c-94f5-c18864c33b64/trunk%402

---
+++ testing/testing.py
@@ -0,0 +1,7 @@
+from testing_python import checker
+
+mycheck = checker.checker()
+mycheck.project = "libMPP"
+# Destinataires du mail
+mycheck.recipients = ['didier.boldo@ign.fr','olivier.tournaire@ign.fr']
+mycheck.check()
