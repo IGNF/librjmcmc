fcf19eb95c2d9049ef6719d23526774297253537
dboldo@6a04c5be-4793-4b4c-94f5-c18864c33b64 <dboldo@6a04c5be-4793-4b4c-94f5-c18864c33b64>
2008-11-17 09:42:38 +0000

    MAJ script python
    --HG--
    extra : convert_revision : svn%3A6a04c5be-4793-4b4c-94f5-c18864c33b64/trunk%406

--- testing/testing.py
+++ testing/testing.py
@@ -1,7 +1,5 @@
 from testing_python import checker
 
-mycheck = checker.checker()
-mycheck.project = "libMPP"
+mycheck = checker.checker("libMPP", ['didier.boldo@ign.fr','olivier.tournaire@ign.fr'] )
 # Destinataires du mail
-mycheck.recipients = ['didier.boldo@ign.fr','olivier.tournaire@ign.fr']
 mycheck.check()
