5f59bdbc0aaf36a5690d6da3fc060f7f092c0396
mbredif@6a04c5be-4793-4b4c-94f5-c18864c33b64 <mbredif@6a04c5be-4793-4b4c-94f5-c18864c33b64>
2010-01-27 12:14:37 +0000

    gradient_image
    --HG--
    extra : convert_revision : svn%3A6a04c5be-4793-4b4c-94f5-c18864c33b64/trunk%40134

---
+++ samples/building_footprint_extraction/core/gradient_image.hpp
@@ -0,0 +1,28 @@
+#ifndef IMAGE_HPP_
+#define IMAGE_HPP_
+
+#include <boost/gil/extension/io/tiff_dynamic_io.hpp>
+#include <boost/gil/extension/matis/float_images.hpp>
+#include "core/bbox.hpp"
+#include "geometry/geometry.h"
+
+namespace rjmcmc {
+class gradient_image
+{
+public:
+	typedef boost::gil::dev2n32F_image_t image_t;
+	typedef boost::gil::dev2n32F_pixel_t pixel_t;
+
+	double integrated_flux(const Segment_2 &s) const;
+	double integrated_flux(const Circle_2  &c) const;
+
+	void load(const std::string &file, double sigmaD=1, unsigned int step=0);
+	void load(const std::string &file, const bbox_2& bbox, double sigmaD=1, unsigned int step=0); // subimage loading
+
+private:
+	image_t m_gradients;
+	bbox_2 m_bbox;
+};
+};
+
+#endif // IMAGE_HPP_
