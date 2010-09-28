9f4813ae7b5a18b4cfbedf5f7dea00d3dd983f1e
"Olivier Tournaire ext:(%22) <olitour@gmail.com>
2010-09-28 13:51:40 +0200

    Add documentation files

---
+++ doc/src/test.cpp
@@ -0,0 +1,22 @@
+//[headers
+#include <iostream>
+
+/*`
+Some trailing text here
+*/
+//]
+
+//[main
+int main(char**argv, int argc)
+{
+//]
+	//[step1
+	int i;
+	//]
+	//[step2
+	i = 0;
+	std::cout << i << std::endl;
+	//]
+//[main2
+}
+//]
