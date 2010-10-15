ce2e11a02b9b5677fd8e44faa64e47374f1fa499
"Olivier Tournaire ext:(%22) <olitour@gmail.com>
2010-10-15 14:10:08 +0200

    doc --> minor fixes in schedule (doxygen); szu and hartley doxygen documentation

--- lib/rjmcmc/sampler/szu_hartley_sampler.hpp
+++ lib/rjmcmc/sampler/szu_hartley_sampler.hpp
@@ -5,7 +5,13 @@
 
 namespace rjmcmc
 {
-
+    /**
+     * \ingroup GroupSampler
+     *
+     * This class is a model of the Sampler concept and implements <i>Szu and Hartley</i> fast annealing. This is the first variant proposed variant of the classical Metropolis acceptance criterion.
+     * \f[P_{SH}=\frac{1}{1+\exp\left(\Delta E / T\right)}\f]
+     * This probability is always less than the Metropolis one, the difference becoming negligible for large \f[\Delta E / T\f].
+     */
     template<typename CountSampler, RJMCMC_TUPLE_TYPENAMES>
     class szu_hartley_sampler
         : public sampler_base<szu_hartley_sampler<CountSampler,RJMCMC_TUPLE_TYPES>, CountSampler, RJMCMC_TUPLE_TYPES>
