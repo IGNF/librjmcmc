af915f4813128ce9f800ba6b81aa28057ffde5d6
mathieu.bredif@ign.fr <mathieu.bredif@ign.fr>
2011-10-20 17:20:39 +0200

    removed salamon initial schedule at gilviewer plugin start up

--- samples/building_footprint_rectangle/gilviewer/building_footprint_rectangle_gilviewer.cpp
+++ samples/building_footprint_rectangle/gilviewer/building_footprint_rectangle_gilviewer.cpp
@@ -158,8 +158,8 @@ public:
         create_schedule     (p,m_schedule);
         create_end_test     (p,m_end_test);
 
-        std::cout << "Salamon initial schedule : " << salamon_initial_schedule(m_sampler->density(),*m_config,1000) << std::endl;
-        m_config->clear();
+    //    std::cout << "Salamon initial schedule : " << salamon_initial_schedule(m_sampler->density(),*m_config,1000) << std::endl;
+    //    m_config->clear();
 
         any_sampler *sampler = new any_sampler(*m_sampler);
 
